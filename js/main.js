6ea63f1d5683518a5bdd9c53896e342eeff6029f
f1rstf1y9 <f1rstf1y9@naver.com>
2022-07-11 23:29:05 +0900

    Add the Header and Dropdown Menu 오픈그래프, 트위터 카드를 추가하고 헤더와 드롭다운 메뉴를 구현

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function(){
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function(){
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function(){
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+const badgeEl = document.querySelector('header .badges');
+window.addEventListener('scroll', _.throttle(function(){
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // 배지 숨기기
+    // gsap.to(애니메이션 처리를 할 요소, 요소가 애니메이션 처리되는 지속 시간, 옵션 );
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    // 배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+// _.throttle(함수, 시간)
